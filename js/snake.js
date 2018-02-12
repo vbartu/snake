4f7cebaec3a8701f35431b9ae163ad7ca034a892
vbartu <vicentebartualf@gmail.com>
2018-02-13 00:54:48 +0100

    added snake length

--- js/snake.js
+++ js/snake.js
@@ -1,25 +1,51 @@
-
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 ctx.scale(20, 20);
 
-var x = 0;
-var y = 0;
+var moveDir = 'down'
+var x = 3, y = 10; // head position
+var l = 6; // snake length
+var tx = x, ty = y - l; //tail position
+var tailMov = ['down', 'down', 'down', 'down', 'down', 'down'];
+
 var canvasWidth = canvas.width / 20;
 var canvasHeight = canvas.height / 20;
 
 ctx.fillStyle = '#FF0000';
-ctx.fillRect(x, y, 1, 1);
+for (var i = y; i >= ty; i--) {
+  ctx.fillRect(x, i, 1, 1);
+}
+
 
 document.addEventListener('keydown', function(event) {
-  if (event.keyCode == 37) moveLeft();
-  else if (event.keyCode == 38) moveUp();
-  else if (event.keyCode == 39) moveRigth();
-  else if (event.keyCode == 40) moveDown();
+  if (event.keyCode == 37) {if (moveDir != 'right') moveDir = 'left';}
+  else if (event.keyCode == 38) {if (moveDir != 'down') moveDir = 'up';}
+  else if (event.keyCode == 39) {if (moveDir != 'left') moveDir = 'right';}
+  else if (event.keyCode == 40) {if (moveDir != 'up') moveDir = 'down';}
 });
 
+function move() {
+  switch (moveDir) {
+    case 'up':
+      moveUp();
+      break;
+
+    case 'down':
+      moveDown();
+      break;
+
+    case "left":
+      moveLeft();
+      break;
+
+    case "right":
+      moveRight();
+      break;
+  }
+}
+
 function moveLeft() {
-  ctx.clearRect(x, y, 1, 1);
+  clearTail();
 
   x--;
   if (x < 0)
@@ -29,7 +55,7 @@ function moveLeft() {
 }
 
 function moveUp() {
-  ctx.clearRect(x, y, 1, 1);
+  clearTail();
 
   y--;
   if (y < 0)
@@ -38,8 +64,8 @@ function moveUp() {
   ctx.fillRect(x, y, 1, 1);
 }
 
-function moveRigth() {
-  ctx.clearRect(x, y, 1, 1);
+function moveRight() {
+  clearTail();
 
   x++;
   if (x >= canvasWidth)
@@ -49,11 +75,47 @@ function moveRigth() {
 }
 
 function moveDown() {
-  ctx.clearRect(x, y, 1, 1);
+  clearTail();
 
   y++;
   if (y >= canvasHeight)
-    y = y - canvasWidth;
+    y = y - canvasHeight;
 
   ctx.fillRect(x, y, 1, 1);
 }
+
+function clearTail() {
+  ctx.clearRect(tx, ty, 1, 1);
+
+  var tm = tailMov[0];
+  switch (tm) {
+    case 'left':
+      tx--;
+      if (tx < 0)
+        tx = canvasWidth + tx;
+      break;
+    case 'up':
+      ty--;
+      if (ty < 0)
+        ty = canvasHeight + ty;
+      break;
+    case 'right':
+      tx++;
+      if (tx >= canvasWidth)
+        tx = tx - canvasWidth;
+      break;
+    case 'down':
+      ty++;
+      if (ty >= canvasHeight)
+        ty = ty - canvasHeight;
+      break;
+
+  }
+
+  for (var i = 1; i < tailMov.length; i++) {
+    tailMov[i-1] = tailMov[i];
+  }
+  tailMov[tailMov.length-1] = moveDir;
+}
+
+var t = setInterval(move, 100);
